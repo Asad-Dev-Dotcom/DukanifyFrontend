57fbcc0aaf143a9932c516d4d0fefdcb63406a17
Nouman <noumanfarooq387@gmail.com>
2025-07-24 21:16:15 +0500

    Signup Feature

--- src/app/auth/service/Signup-Service/signup.service.ts
+++ src/app/auth/service/Signup-Service/signup.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http:HttpClient) { }
+  
+  register(payload:any)  {
+    return this.http.post(`${this.apiUrl}/signup`, payload);
+}
 
-  constructor() { }
+
+
 }
