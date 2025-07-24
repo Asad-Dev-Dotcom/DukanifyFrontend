a6bd32901672345ae8f67b91373bbc4d2bb204dd
Rehman Khan <rehmantayyabkhan@gmail.com>
2025-07-24 22:07:36 +0500

    Signup Verify OTP Tayyab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VerifyEmailComponent } from './auth/Signup/verify-email/verify-email.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:VerifyEmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
