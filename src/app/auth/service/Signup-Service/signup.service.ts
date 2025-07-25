434e1347d1f8cfb999a7aa48a9449c2e4baecc81
Nouman <noumanfarooq387@gmail.com>
2025-07-25 22:47:25 +0500

    signup add field name

--- src/app/auth/service/Signup-Service/signup.service.ts
+++ src/app/auth/service/Signup-Service/signup.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment'
+import { environment } from '../../../../environments/environment';
 import { AuthResponse } from '../../model/auth-response.model';
 import { Observable } from 'rxjs';
+
+export interface SignupPayload {
+  name: string;       // ✅ Name added here
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
   private apiUrl = environment.apiUrl;
-  constructor(private http: HttpClient) { }
 
-  register(payload: any) {
+  constructor(private http: HttpClient) { }
+ 
+  register(payload: SignupPayload): Observable<any> {
     return this.http.post(`${this.apiUrl}/signup`, payload);
   }
 
@@ -27,5 +36,5 @@ export class SignupService {
       { withCredentials: true }
     );
   }
-
 }
+
